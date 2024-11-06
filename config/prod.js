6d2ed2cfd26429e4aa171b414f0d96b41d2688f4
jihong <hongs7803@gmail.com>
2024-11-06 18:40:40 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: this.MONGO_URI,
+};
+// heroku 서비스 사용시
--- index.js
+++ index.js
@@ -2,17 +2,45 @@
 const express = require("express"); // express module 가져오기
 const app = express(); // 앱을 만들기
 const port = 5001; // 5001번 포트를 사용
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User"); // User 갖고오기
+
+// application/x-www-form-urlencoded 분석해서 가져옴
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 분석해서 가져올 수 있게 함.
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://hongs7803:ItIycxt8l6P5VY6s@cluster0.lfn0f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World! 섹스");
+  res.send("Hello World! 섹스조아");
+});
+
+app.post("/register", (req, res) => {
+  //클라이언트에서 보내주는 회원가입 정보들을 client에서 가져오면
+  //그것들을 db에 넣는다.
+
+  const user = new User(req.body); // 정보들이 user모델에 저장됨
+
+  user
+    .save()
+    .then((userInfo) => res.status(200).json({ success: true }))
+    .catch((err) => res.json({ success: false, err }));
+
+  //  user.save()는 더 이상 콜백을 인자로 받지 않게 됨.
+  //   user.save((err, doc) => {
+  //     if (err) return res.json({ success: false, err });
+  //     return res.status(200).json({
+  //       success: true,
+  //     }); // 성공을 뜻함.
+  //   });
 });
 
 app.listen(port, () => {
